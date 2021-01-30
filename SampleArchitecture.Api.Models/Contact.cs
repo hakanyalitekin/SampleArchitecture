5d437405ae08cc9048fe50120bd031a839a06b2e
Hakan Yalitekin <hyalitekin@tekzen.com.tr>
2021-01-30 23:10:10 +0300

    Project version upgrade

---
+++ SampleArchitecture.Api.Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleArchitecture.Api.Models
+{
+   public class Contact
+    {
+        public int Id { get; set; }
+        public String FullName { get; set; }
+    }
+}
