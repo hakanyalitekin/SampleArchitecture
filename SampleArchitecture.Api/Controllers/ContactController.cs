04f28d520b75e50fe09286da723ef88cf2bd7dc0
Hakan Yalitekin <hyalitekin@tekzen.com.tr>
2021-01-31 15:15:29 +0300

    Ortama Bağlı Ayarlar - Configurations completed

---
+++ SampleArchitecture.Api/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleArchitecture.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ContactController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public string Get()
+        {
+            return _configuration["ReadMe"].ToString();
+        }
+    }
+}
--- SampleArchitecture.Api/Program.cs
+++ SampleArchitecture.Api/Program.cs
@@ -11,6 +11,22 @@ namespace SampleArchitecture.Api
 {
     public class Program
     {
+        private static IConfiguration Configuration
+        {
+            get
+            {
+                // Ortamýmýzý belirleyen Evniroment deðiþkenimizi okuyoruz. Buna göre çalýþýlacak ortam aylarýný ilgili json'dan okuyoruz.
+                string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                return new ConfigurationBuilder()
+                    .SetBasePath(System.IO.Directory.GetCurrentDirectory()) // Bu uygulamanýn ayarlarýnýn yükleneceði klasör, bu uygulamanýn içerisi.
+                    .AddJsonFile("appsettings.json", optional: false) // Eðer hiçbir þey bulamazsa bunu kullan demek "optional:false"
+                    .AddJsonFile($"appsettings.{env}.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+            }
+        }
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -21,6 +37,8 @@ namespace SampleArchitecture.Api
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseConfiguration(Configuration); // Uygulamamýz çalýþtýðýnda gelip yukarýdaki ayarlarýmýzý okumasý için ekledik.
+
                 });
     }
 }
